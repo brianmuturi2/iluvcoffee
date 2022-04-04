1815cc169df207dfebb9b3e695b2855c48ec1b85
brianmuturi2 <brianmuturi2@gmail.com>
2022-04-04 15:44:36 +0300

    feat(interceptor): implemented interceptor to transform response body

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { ApiKeyGuard } from './guards/api-key.guard';
 import { ConfigModule } from '@nestjs/config';
+import { WrapResponseInterceptor } from './interceptors/wrap-response.interceptor';
 
 @Module({
   imports: [ConfigModule],
   providers: [
     {
       provide: APP_GUARD, useClass: ApiKeyGuard
-    }
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: WrapResponseInterceptor,
+    },
   ]
 })
 export class CommonModule {}
